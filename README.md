d55fea2a4e9c06136c024d61d852606afa5cb017
AlexandreArduino <alexandre10baalbaky@gmail.com>
2021-05-30 12:09:40 +0200

    CSV object is done!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyNeural
-This module is an idea to predict datas from a csv file
+This module is an idea to predict datas from a csv file.
+
+# Note
+
+    For the network I'm not using Tensorflow or others module. I tried to code it from scratch. 
---
+++ _csv.py
@@ -0,0 +1,31 @@
+class CSV(object):
+    def __init__(self, path):
+        print("[WARNING] CSV module can't read more than one column in a CSV file!")
+        self.CorrectFile = False
+        self.datas = []
+        if not self.IsCorrectFile(path):
+            print("[CSV] Incorrect file/path!")
+            exit()
+        else: self.path = path
+    def IsCorrectFile(self, path):
+        try:
+            file = open(path, "r")
+            file.close()
+            self.CorrectFile = True
+            return True
+        except: return False
+    def ReadData(self):
+        if not self.CorrectFile:
+            print("You can't read datas from no file!")
+            exit()
+        else:
+            file = open(self.path, "r")
+            l = file.readlines()
+            file.close()
+            for i in range(0, len(l)):
+                l[i] = l[i].replace("\n", "")
+                try:
+                    l[i] = int(l[i])
+                except: del l[i]
+            self.datas = l
+        return self.datas #Keeping self.datas in memory for others functions if it is needing
