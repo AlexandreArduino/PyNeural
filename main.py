901f0ce27c8ce3cc5932be1fc4d691727d473b32
AlexandreArduino <alexandre10baalbaky@gmail.com>
2021-05-30 13:01:36 +0200

    Able to do the whole code

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from PyNeural import PyNeural
 
 network = PyNeural("datasets/covid_france.csv")
-network.Train()
-print(network.PredictValue())
+datas = network.inputs
+del network
+for i in range(10):
+    network = PyNeural("datasets/covid_france.csv")
+    network.Train()
+    datas.append(network.PredictValue())
+    print(str(datas))
+    del network
+    file = open("datasets/covid_france.csv", "w")
+    for j in range(0, len(datas)):
+        file.write(str(datas[i]) + "\n")
+    file.close()
